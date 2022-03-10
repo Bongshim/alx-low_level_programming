7ffb2289c83fefb0b09505f922220d3912cf73fd
Bongshim <bongshime@gmail.com>
2022-03-10 10:57:37 -0500

    output to stderr

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+  * main - prints a character using fprintf
+  *
+  * Return: returns 0
+  */
+
+int main(void)
+{
+	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+
+	fprintf(stderr, "%s\n", str);
+
+	return (0);
+}

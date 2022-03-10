66b10e7d78c96586e0ed7a29b0af15e47f37b8de
Bongshim <bongshime@gmail.com>
2022-03-10 07:51:06 -0500

    using Puts in C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,9 +3,9 @@
 
 /**
   * main: Using the puts function to print a string
+  * puts: inserts the character
   *
   * Description: The program displays a string using puts
-  * puts: inserts the character
   * Return: returns 0
   */
 int main(void)

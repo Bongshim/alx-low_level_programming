75270177da66ff49fb2a37f1a6fb4668e400f405
Bongshim <bongshime@gmail.com>
2022-03-10 08:09:06 -0500

    using Printf in C

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+  * main - prints a character using printf 
+  *
+  * Return: returns 0
+  */
+
+int main(void)
+{
+	char str = "with proper grammar, but the outcome is a piece of art,";
+
+	printf("%c\n", str);
+
+	return (0);
+}

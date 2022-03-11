0b65c8793399e5d99144494f775de512b6149ad1
Bongshim <bongshime@gmail.com>
2022-03-11 11:12:38 -0500

    using putchar to print all alphabet in lower case except q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+  * main - main function
+  *
+  * Return: returns an int 0
+  */
+
+int main(void)
+{
+	char str;
+
+	for (str = 'a'; str <= 'z'; str++)
+	{
+		if (str != q || str != e)
+		{
+			putchar(str);
+		}
+
+		
+	}
+	putchar('\n');
+	return (0);
+
+}

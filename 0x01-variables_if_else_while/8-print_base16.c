825cfb9198047b308b1b2ff834209172dfd8947c
Bongshim <bongshime@gmail.com>
2022-03-11 11:54:12 -0500

    print alphabet in reverse and lowercase

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,10 +9,15 @@
 int main(void)
 {
 	int n;
+	char s;
 
-	for (n = 0; n <= 15; n++)
+	for (n = 0; n <= 9; n++)
 	{
 		putchar(n % 10 + '0');
+	}
+	for (s = 'a'; s <='f'; s++)
+	{
+		putchar(s);
 	}
 	putchar('\n');
 	return (0);

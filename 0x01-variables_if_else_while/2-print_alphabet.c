c7e5cb9d0459fa6b70319551d8ad59ca660fd96a
Bongshim <bongshime@gmail.com>
2022-03-11 10:52:06 -0500

    using putchar to print all alphabet in lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+  * main - main function
+  *
+  * Return: returns an int 0
+  */
+
+int main(void)
+{
+	char str;
+
+	for (str = 'a'; str <= 'z'; str++)
+	{
+		putchar(str);
+	}
+	return (0);
+
+}

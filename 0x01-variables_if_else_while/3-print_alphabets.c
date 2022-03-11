3ebe0a5cf617025f3cd3697626045ef5bb34ebe7
Bongshim <bongshime@gmail.com>
2022-03-11 11:05:46 -0500

    using putchar to print all alphabet in upper and lower

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+  * main - main function
+  *
+  * Return: returns an int 0
+  */
+
+int main(void)
+{
+	char str;
+	char str2;
+
+	for (str = 'a'; str <= 'z'; str++)
+	{
+		putchar(str);
+	}
+	for (str = 'A'; str =< 'z'; str++)
+	{
+		putchar(str2);
+	}
+	putchar('\n');
+	return (0);
+
+}

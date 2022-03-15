6f2e14dbdd9e9b9214fcc7ae7b0e2f3cf44ab75e
Bongshim <bongshime@gmail.com>
2022-03-15 17:19:18 -0400

    count down

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,13 +11,20 @@ void print_to_98(int n)
 {
 	int c;
 
-	for (c = n; c <= 98; c++)
+	if ( n <= 98)
 	{
-		if (c < 98)
+		for (c = n; c < 98; c++)
 		{
 			printf("%d, ", c);
 		}
-		else
-			printf("98");
+		printf("98");
+	}
+	else
+	{
+		for (c = n; c > 98; c--)
+		{
+			printf("%d, ", c);
+		}
+		printf("98");
 	}
 }

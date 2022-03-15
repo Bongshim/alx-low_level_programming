09ca2ab534fe0d341d8fb2f4dc025c0e4ff4d60e
Bongshim <bongshime@gmail.com>
2022-03-15 14:05:38 -0400

    returns 1 if variable is a letter upper case or lower case

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+  * _islower - checks for the case
+  * @c: first parameter
+  *
+  * Return: returns an int 0
+  */
+
+int _isalpha(int c)
+{
+	return (c >= 'a' && c <= 'z'||c >= 'A' && c <= 'Z');
+}

d3802989ec93055f8641f1e75f60c57adc8b5854
Bongshim <bongshime@gmail.com>
2022-03-15 15:59:26 -0400

    9 times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+  * jack_bauer - checks for the case
+  *
+  * Return: returns an int 0
+  */
+
+void times_table(void)
+{
+	int i
+	for (i = 0; i <= 9; i++)
+	{
+
+		_putchar(i % 10 + '0')
+	}
+}

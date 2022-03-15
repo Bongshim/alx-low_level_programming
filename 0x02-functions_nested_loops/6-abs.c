23d463873df9210b43d71a7f2df5fd38d10b4804
Bongshim <bongshime@gmail.com>
2022-03-15 14:54:57 -0400

    calculating the absolut value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+  * _islower - checks for the case
+  * @c: first parameter
+  *
+  * Return: returns an int 0
+  */
+
+int _abs(int c)
+{
+	if (c >= 0)
+	{
+		return (c);
+	} else
+	{
+		return (0-(c));
+	}
+}

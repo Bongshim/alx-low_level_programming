bbbe29aeda468a3f5cd6ef688636b87da437a0e1
Bongshim <bongshime@gmail.com>
2022-03-15 16:16:17 -0400

    9 times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,12 +8,32 @@
 
 void times_table(void)
 {
-	int i;
-	for (i = 0; i <= 9; i++)
-	{
+	int r, c, d;
 
-		_putchar(i % 10 + '0' );
-		_putchar(44);
-		_putchar(32);
+	for (r = 0; r <= 9; r++)
+	{
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
+	for (c = 1; c <= 9; c++)
+	{
+		d = (r * c);
+	if ((d / 10) > 0)
+	{
+		_putchar((d / 10) + '0');
+	}
+	else
+	{
+		_putchar(' ');
+	}
+		_putchar((d % 10) + '0');
+
+	if (c < 9)
+	{
+		_putchar(',');
+	      _putchar(' ');
+	}
+	}
+	_putchar('\n');
 	}
 }

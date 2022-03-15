eef65ee5c2044ab9cedd3d97bf9e6a6d7923d271
Bongshim <bongshime@gmail.com>
2022-03-15 15:11:31 -0400

    getting the last digit with % 10

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+  * print_last_digit - checks for the case
+  * @c: first parameter
+  *
+  * Return: returns an int 0
+  */
+
+int print_last_digit(int c)
+{
+	return (c % 10);
+}

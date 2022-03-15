59beda92b3436b065d0ce4375d51bd30d4aabe5e
Bongshim <bongshime@gmail.com>
2022-03-15 12:09:07 -0400

    creating a prototype

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "stdio.h"
+
+/**
+  * main - main function
+  *
+  * Return: returns an int 0
+  */
+
+void print_alphabet(void)
+{
+	char alph
+	for (alph = 'a'; alph <= 'z'; alph++)
+	{
+		putchar(alph);
+	}
+	putchar('\n');
+	return (0);
+}

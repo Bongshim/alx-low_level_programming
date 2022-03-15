4f28b1ba8fb939ac922c9c481b267098aecca714
Bongshim <bongshime@gmail.com>
2022-03-15 12:44:46 -0400

    creating a prototype that uses aother prototype to print a value 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+  * print_alphabet - main function
+  *
+  * Return: returns an int 0
+  */
+
+void print_alphabet_x10(void)
+{
+	int i;
+	int j;
+
+	for (j = 0; j < 10; j++)
+	{
+		for (i = 'a'; i <= 'z'; i++)
+			
+		{
+			_putchar(i);
+		}
+		_putchar('\n');
+	}
+}

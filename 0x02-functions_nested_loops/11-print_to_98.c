38dc8ba546489ff0b3c438d52df011fcbcd26937
Bongshim <bongshime@gmail.com>
2022-03-15 17:00:31 -0400

    count down

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+  * print_to_98  - checks for the case
+  * @n: first parameter
+  *
+  * Return: returns an int 0
+  */
+
+void print_to_98(int n)
+{
+	int c;
+
+	for (c = n; c <= 98; c++)
+	{
+		if (c > 98)
+		{
+			_putchar(c % 10 + '0');
+			_putchar(', ');
+		}
+		else
+			_putchar(c % 10 + '0');
+	}
+}

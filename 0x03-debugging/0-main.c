c89e98db540106bd0536a7b3e2193f483a18faf4
Bongshim <bongshime@gmail.com>
2022-03-16 11:17:41 -0400

    debugging

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+  * main - main function that prints if an integer is + or -
+  *
+  * Return: returns an int 0
+  */
+
+int main(void)
+{
+	int i; 
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
+

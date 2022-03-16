1db3611c74a64bec1b4500950a4fe5368ad64df2
Bongshim <bongshime@gmail.com>
2022-03-16 04:24:51 -0400

    sum of all multiples

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,23 @@
+#include <stdio.io>
+
+/**
+  * main - main function
+  *
+  * Return: returns an int 0
+  */
+
+int main(void)
+{
+	int n;
+	int sum = 0;
+
+	for (n = 0; n < 1024; n++)
+	{
+		if ((n % 3 == 0) || (n % 5 == 0))
+		{
+			sum += n;
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}

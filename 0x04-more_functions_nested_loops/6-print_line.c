e76494718194a55e01c1108fe2e53c9a09d5404a
Bongshim <bongshime@gmail.com>
2022-03-17 19:26:00 -0400

    function that prints a line a number of times

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+*print_line - print a line based on the number of input
+*Return: returns a value
+*/
+
+void print_line(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+		for (i = 0; i <= n; i++)
+		{
+			_putchar(95);
+		}
+		_putchar('\n')
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}

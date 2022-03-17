049dc40f7a6d69cb10376e3c8beefc81ea47195c
Bongshim <bongshime@gmail.com>
2022-03-17 18:11:04 -0400

    function that multiplies two digits

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+*mul - function that checks for uppercase characters
+*@a: int type number
+*@b: int type number
+*Return: returns the result
+*/
+
+int mul(int a, int b);
+{
+	return (a * b);
+}

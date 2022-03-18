942d1ae0063608dc74e221d8215f693ce8c4d9c3
Bongshim <bongshime@gmail.com>
2022-03-17 20:24:29 -0400

    fizzbuzz challenge

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+*main - main function
+*Return: returns a value
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if ((i % 3 == 0) && (i % 5 == 0))
+		{
+			printf("FizzBuzz ");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else
+		{
+		printf("%i ", i);
+		}
+	}
+	
+	return (0);
+}

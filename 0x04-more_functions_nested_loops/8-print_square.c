c7d80e717713f1170846c4a94f33e5c560e4050f
Bongshim <bongshime@gmail.com>
2022-03-17 20:04:37 -0400

    function that prints a square with #

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+*print_square - print a line based on the number of input
+*@size: first parameter
+*Return: returns a value
+*/
+
+void print_square(int size)
+{
+	int i, j;
+
+	if (size > 0)
+	{
+		for (i = 0; i < size; i++)
+		{
+			for(j = 0; j < size; j++)
+			{
+				_putchar(35);
+			}
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
